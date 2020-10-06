3bed1f62f62f8630e1792e6fc462b16fc54c2469
Оксана Е. Лисина <olisina@yamoney.ru>
2020-10-06 23:52:14 +0300

    add all

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('a[href^="#"').on('click', function() {
+
+    let href = $(this).attr('href');
+
+    $('html, body').animate({
+        scrollTop: $(href).offset().top
+    });
+    return false;
+});
+
